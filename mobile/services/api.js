0431cec239c590f4486c8d35359019bb5dcbd548
ayoubdev001 <ayoubkhay85@gmail.com>
2026-09-04 15:21:00 +0100

    fix: update project name in package.json and correct API URL in api.js

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,6 +1,6 @@
 
 
-const API_URL = "http://192.168.1.137:3000/api";
+const API_URL = "http://192.168.1.217:3000/api";
 
 export async function getCategories() {
     const response = await fetch(`${API_URL}/categories`);
